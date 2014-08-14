2b52c0a4d7a0c31af67134e1bf145769a72716de
mezuky04 <alexandru.bugarin@gmail.com>
2014-08-14 15:35:37 +0300

    Initial commit

---
+++ BaseModel.php
@@ -0,0 +1,206 @@
+<?php
+
+/**
+ * Class BaseModel
+ *
+ * Implements basic database operations like select, update, delete
+ */
+class BaseModel {
+
+    /**
+     * @var string Table name
+     */
+    protected $_tableName = '';
+
+    /**
+     * @var array Table fields
+     */
+    protected $_tableFields = array();
+
+    /**
+     * @var null Database instance
+     */
+    private $_query = null;
+
+
+    /**
+     * Check if all properties are set
+     */
+    public function __construct() {
+        if (!$this->_tableName) {
+            throw new Exception("Undefined table name");
+        }
+        if (!count($this->_tableFields)) {
+            throw new Exception("Undefined table fields");
+        }
+    }
+
+
+    /**
+     * Get one record that match the given $filter
+     *
+     * @param array $filter key => value pairs to use in WHERE clause
+     * @param array $fields To use in SELECT clause
+     * @return object
+     */
+    public function getOne($filter = array(), $fields = array()) {
+
+        $this->_query = DB::table($this->_tableName);
+
+        // Clean fields
+        $this->_cleanFields($fields);
+
+        // Add select fields
+        $this->_buildSelect($fields);
+
+        // Build where
+        $this->_buildWhere($filter);
+
+        return $this->_query->first();
+    }
+
+
+    /**
+     * Get all records that match the given $filter
+     *
+     * @param array $filter key => value pairs to use in WHERE clause
+     * @param array $fields To use in SELECT clause
+     * @return mixed
+     */
+    public function getAll($filter = array(), $fields = array()) {
+
+        $this->_query = DB::table($this->_tableName);
+
+        // Clean fields
+        $this->_cleanFields($fields);
+
+        // Add select fields
+        $this->_buildSelect($fields);
+
+        // Build where
+        $this->_buildWhere($filter);
+
+        return $this->_query->get();
+    }
+
+
+    /**
+     * Insert one or more records
+     *
+     * @param array $record key => value pairs
+     */
+    public function insert($record = array()) {
+
+        $this->_query = DB::table($this->_tableName);
+
+        if (!count($record)) {
+            return;
+        }
+        $this->_query->insert($record);
+    }
+
+
+    /**
+     * Insert one record and get their id
+     *
+     * @param array $record key => value pairs
+     * @return int Insert id
+     */
+    public function insertGetId($record = array()) {
+
+        $this->_query = DB::table($this->_tableName);
+
+        if (!count($record)) {
+            return;
+        }
+        return $this->_query->insertGetId($record);
+    }
+
+
+    /**
+     * Update records that match the given filter
+     *
+     * @param array $record key => value pairs
+     * @param array $filter key => value pairs to be used in WHERE clause
+     */
+    public function update($record = array(), $filter = array()) {
+
+        $this->_query = DB::table($this->_tableName);
+
+        // Build where
+        $this->_buildWhere($filter);
+
+        $this->_query->update($record);
+    }
+
+
+    /**
+     * Delete records that match the given filter
+     *
+     * @param array $filter key => value pairs to use in WHERE clause
+     */
+    public function delete($filter = array()) {
+
+        $this->_query = DB::table($this->_tableName);
+
+        // Build where
+        $this->_buildWhere($filter);
+
+        $this->_query->delete();
+    }
+
+
+    /**
+     * Truncate a table
+     */
+    public function truncate() {
+
+        $this->_query = DB::table($this->_tableName);
+
+        $this->_query->truncate();
+    }
+
+
+    /**
+     * Build select clause
+     *
+     * @param array $fields
+     */
+    private function _buildSelect($fields = array()) {
+        if (!count($fields)) {
+            return;
+        }
+        foreach ($fields as $field) {
+            $this->_query->select($field);
+        }
+    }
+
+
+    /**
+     * Build where clause
+     *
+     * @param array $filter key => value pairs
+     */
+    private function _buildWhere($filter = array()) {
+        if (!count($filter)) {
+            return;
+        }
+        foreach ($filter as $key => $value) {
+            $this->_query->where($key, $value);
+        }
+    }
+
+
+    /**
+     * Remove unknown fields
+     *
+     * @param array $fields
+     */
+    private function _cleanFields(&$fields) {
+        foreach ($fields as $key => $field) {
+            if (!in_array($field, $this->_tableFields)) {
+                unset($fields[$key]);
+            }
+        }
+    }
+}
